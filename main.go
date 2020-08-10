9588f775ff44855c33d72f232ac857b7a38e4866
Michael Friedrich <mfriedrich@gitlab.com>
2020-08-10 19:51:43 +0200

    Import Golang Tanuki

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	hello_from := os.Getenv("HELLO_FROM")
+
+	if hello_from == "" {
+		hello_from = "our GitLab meetup"
+	}
+
+	fmt.Println("Hello from " + hello_from + "!")
+	fmt.Println("Today we learn about GitLab Best Practices and CI/CD magic :)")
+
+	fmt.Println("");
+
+	fmt.Print(GetTanuki(true))
+
+	fmt.Println("");
+	fmt.Println("Join us at https://www.everyonecancontribute.com")
+}
